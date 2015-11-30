a0b946e7244cc669f82c668af95a701137560a9c
Hugo Forte <hforte@gcmlp.com>
2015-11-30 11:44:37 -0600

    One Failing Test

---
+++ Application/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Server : IServer
+    {
+        public string TakeOrder(string order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IServer
+    {
+        string TakeOrder(string order);
+
+    }
+}
---
+++ ApplicationTests/ServerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Application;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+
+namespace ApplicationTests
+{
+    [TestFixture]
+    public class ServerTests
+    {
+        private Server _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Server();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+
+        }
+
+        [Test]
+        public void CanServeSteak()
+        {
+            var order = "1";
+            string expected = "steak";
+            var actual = _sut.TakeOrder(order);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
