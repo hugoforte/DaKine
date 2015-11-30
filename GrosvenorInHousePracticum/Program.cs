2284f84c3f23c3244945f6a467f07f62b94aac07
Hugo Forte <hforte@gcmlp.com>
2015-11-30 12:36:41 -0600

    Finished

--- GrosvenorInHousePracticum/Program.cs
+++ GrosvenorInHousePracticum/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application;
 
 namespace GrosvenorInHousePracticum
 {
@@ -10,6 +11,16 @@ namespace GrosvenorInHousePracticum
     {
         static void Main(string[] args)
         {
+
+            var server = new Server(new DishManager());
+            while (true)
+            {
+
+                var order = Console.ReadLine();
+                var output = server.TakeOrder(order);
+                Console.WriteLine(output);
+
+            }
         }
     }
 }
